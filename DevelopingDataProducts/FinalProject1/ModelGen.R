7f547a61c54d47ce2a440b02c29034d1497121a4
Jason Foster <c-jason.foster@skykick.com>
2017-03-02 12:57:40 -0700

    Correcting errors

--- DevelopingDataProducts/FinalProject1/ModelGen.R
+++ DevelopingDataProducts/FinalProject1/ModelGen.R
@@ -1,11 +1,35 @@
-suppressMessages(library(dplyr))
-suppressMessages(library(caret))
+assertPackage <- function(pkg) {
+  if (!suppressMessages(require(pkg, character.only = TRUE, quietly = TRUE))) {
+    install.packages(pkg, dep=TRUE)
+    if (!suppressMessages(require(pkg, character.only = TRUE))) {
+      stop("Package not found")
+    }
+  }
+}
+
+setWdToCurrent <- function() {
+  setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) 
+}
 
 set.seed(13031)
 
-download.file("https://archive.ics.uci.edu/ml/machine-learning-databases/00222/bank.zip", destfile="bank.zip")
-unzip("bank.zip")
-bank <- read.csv("bank-full.csv", sep = ";")
+setWdToCurrent()
+
+assertPackage("dplyr")
+assertPackage("caret")
+
+if (!file.exists("bank.zip")) {
+  download.file("https://archive.ics.uci.edu/ml/machine-learning-databases/00222/bank.zip", 
+                destfile="bank.zip")
+  unzip("bank.zip")
+}
+
+if (!file.exists("bank-full.rds")) {
+  bank <- read.csv("bank-full.csv", sep = ";")
+  saveRDS(bank, file = "bank-full.rds")
+} else {
+  bank <- readRDS("bank-full.rds")
+}
 
 # Use dplyr to remove the duration column, based on guidance of the data guide on UCI which
 # recommends excluding the duration
@@ -20,8 +44,8 @@ saveRDS(testing, file="TestingData.rds")
 
 # Generate the model and save it so we can use it on the back-end without regenning it. Here we
 # utilize all the cores on the machine, just so we can generate the model quicker
-if (!file.exists("gbmModel.RData")) {
-  suppressMessages(library(doParallel))
+if (!file.exists("./BankMarketing/gbmModel.RData")) {
+  assertPackage("doParallel")
   registerDoParallel(detectCores(all.tests=TRUE, logical=TRUE))
   gbmModel <- train(y ~ poutcome + previous + campaign + age + balance, 
                     data=training, 
@@ -29,12 +53,12 @@ if (!file.exists("gbmModel.RData")) {
                     preProcess=c("center","scale"),
                     trControl=trainControl(method="cv", number=10, classProbs = TRUE),
                     tuneGrid=expand.grid(.n.trees = c(50), .interaction.depth = c(2), .shrinkage = c(.1), .n.minobsinnode = c(100)))
-  save(gbmModel, file="gbmModel.RData")
+  save(gbmModel, file="./BankMarketing/gbmModel.RData")
   stopImplicitCluster()
   registerDoSEQ()
   detach("package:doParallel")
 } else {
-  load("gbmModel.RData")
+  load("./BankMarketing/gbmModel.RData")
 }
 
-
+gbModel
